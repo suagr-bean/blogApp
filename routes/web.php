a1aad8cde1638ea865048bd0d03d534b5d3c42c3
欧辉 <o1135839128h@gmail.com>
2025-11-24 10:30:34 +0000

    first

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\BlogController;
-Route::get('/text', function () {
-    return view('text');
- });
-Route::get('/home',function(){
-    return view('home');
-});
-Route::get('/range',function (){
-    return view ('range');
-});
+
 Route::get("/send",function(){
     return view("create");
 });
